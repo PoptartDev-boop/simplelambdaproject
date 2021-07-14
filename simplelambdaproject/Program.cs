f098174cdd3721e99dbf3b7b7c8ea081a8136784
LordOfKeyboards <73976765+PoptartDev-boop@users.noreply.github.com>
2021-07-14 10:02:22 -0700

    Add project files.

---
+++ simplelambdaproject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace simplelambdaproject
+{
+    class Program
+    {
+        //Variables
+        delegate void myDelegate(int a, int b);
+        static myDelegate MyDelegate;
+
+        static void Main(string[] args)
+        {
+            //Lambda expression
+            MyDelegate = (num1, num2) =>
+            {
+                //Result is adding the value of 'a' 2 times aswell with the 'b'
+                var result = (num1 + num1) + (num2 + num2);
+                Console.WriteLine(result);
+            };
+            
+            //a is set to 5 and b is set to 10.
+            MyDelegate(5, 10);
+
+            //to make sure the console doesnt close unless a key is pressed
+            Console.ReadKey();
+        }
+    }
+}
